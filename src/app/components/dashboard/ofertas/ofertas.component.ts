0ec234236861db981a84278686c630291e2ae482
ErickChamorro <eschamorro97@misena.edu.co>
2018-11-26 14:25:46 -0500

    implemente el reactive form para ofertas component

--- src/app/components/dashboard/ofertas/ofertas.component.ts
+++ src/app/components/dashboard/ofertas/ofertas.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FuncionesService } from 'src/app/servicios/funciones.service';
+import { FormBuilder, FormGroup } from '@angular/forms';
 declare var $: any;
 @Component({
   selector: 'app-ofertas',
@@ -8,9 +9,21 @@ declare var $: any;
 })
 export class OfertasComponent implements OnInit {
 
-  constructor(private funciones: FuncionesService) {}
+  ofertas_form: FormGroup;
+  constructor(public funciones: FuncionesService, private formBuilder: FormBuilder) {
+    this.ofertas_form = this.formBuilder.group({
+      producto: [],
+      precio: [],
+      imagen: [],
+      descripcion: []
+    });
+  }
 
   ngOnInit() {
     this.funciones.ver_nombre_imagen();
+  }
+
+  submit_handler() {
+    console.log(this.ofertas_form.value);
   }
 }
