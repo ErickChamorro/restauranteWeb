82525c56d057e6388a893e74608f0e7529804e00
ErickChamorro <eschamorro97@misena.edu.co>
2018-11-29 12:09:37 -0500

    ya hace login e ingresa al aplicativo

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -4,6 +4,7 @@ import { Location } from '@angular/common';
 import { Router } from '@angular/router';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import swal from 'sweetalert2';
+import { ApiService } from '../../servicios/apis/api.service';
 
 
 @Component({
@@ -18,19 +19,28 @@ export class HomeComponent implements OnInit {
     private formBuilder: FormBuilder,
     private http: HttpClient,
     public location: Location,
-    public router: Router) {
-      this.loginForm = this.formBuilder.group({
-        usuario: [''],
-        password: ['']
-      });
-    }
+    public router: Router,
+    public apiService: ApiService) {
+    this.loginForm = this.formBuilder.group({
+      username: [''],
+      password: ['']
+    });
+  }
 
   ngOnInit() {
   }
 
   submit_handler() {
-    console.log(this.loginForm.value);
-    this.router.navigate(['/dashboard']);
+    this.apiService.login(this.loginForm.value)
+      .subscribe(respuesta => {
+        const token = respuesta['access_token'];
+        localStorage.setItem('token', token);
+        this.router.navigate(['/dashboard']);
+      }, error => {
+        console.log(error);
+      });
+    // console.log(this.loginForm.value);
+    // this.router.navigate(['/dashboard']);
   }
 
 }
